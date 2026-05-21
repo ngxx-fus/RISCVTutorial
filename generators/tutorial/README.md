b64076ac12e1f7da0e15dfbb2a5a537dea51070e
uec-hanken <trongthuc.hoang@gmail.com>
2026-05-21 19:36:57 +0900

    Add tutorial module

---
+++ generators/tutorial/README.md
@@ -0,0 +1,3 @@
+# Tutorial ROCC module
+
+This is a module for teaching and experimentation, based on the SHA3 RoCC Accelerator
---
+++ generators/tutorial/software/tests/src/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "encoding.h"
+#include "compiler.h"
+
+int main() {
+  printf("Hello world from PHAM Lab\r\n");
+  printf("Student name: xxx_xxx_xxx\r\n");
+  printf("Config: RVxxIxxxx, x KB D-Cache, x KB-ICache\r\n");
+  return 0;
+}
+
+
